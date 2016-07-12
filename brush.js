3659c5f518c9bd717d8d6b59a155c906842fab21
YuanZhaokang <2503144553@qq.com>
2016-07-12 14:44:56 +0800

    刷选与g2结合

--- brush.js
+++ brush.js
@@ -1,20 +1,44 @@
 $(function () {
-    console.log("exe");
-    //日期的添加
-    var year = ["2011", "2012", "2013", "2014", "2015", "2016"];
-    var month = ["2015-01", "2015-02", "2015-03", "2015-04", "2015-05", "2015-06", "2015-07", "2015-08", "2015-09", "2015-10", "2015-11", "2015-12", "2016-01", "2016-02", "2015-03"];
-    
-    draw(year);
+    var data = [
+        { genre: 'Sports', sold: 275 },
+        { genre: 'Strategy', sold: 115 },
+        { genre: 'Action', sold: 120 },
+        { genre: 'Shooter', sold: 350 },
+        { genre: 'Other', sold: 150 },
+    ]; // G2 对数据源格式的要求，仅仅是 JSON 数组，数组的每个元素是一个标准 JSON 对象。
 
-    $("#year").on("click", function () {
-        draw(year);
+    var da = [];
+    data.forEach(function (d, i) {
+        da.push(d["sold"]);
     });
 
-    $("#month").on("click", function () {
-        draw(month);
-    });
+    drawData(data);
+    drawBrush(da.sort());
 
-    function draw(date) {
+    function drawData(data) {
+        d3.select("#c1").selectAll("*").remove();
+        // Step 1: 创建 Chart 对象
+        var chart = new G2.Chart({
+            id: 'c1', // 指定图表容器 ID
+            width: 800, // 指定图表宽度
+            height: 400 // 指定图表高度
+        });
+        // Step 2: 载入数据源
+        chart.source(data, {
+            genre: {
+                alias: '游戏种类' // 列定义，定义该属性显示的别名
+            },
+            sold: {
+                alias: '销售量'
+            }
+        });
+        // Step 3：创建图形语法，绘制柱状图，由 genre 和 sold 两个属性决定图形位置，genre 映射至 x 轴，sold 映射至 y 轴
+        chart.interval().position('genre*sold').color('genre')
+        // Step 4: 渲染图表
+        chart.render();
+    }
+
+    function drawBrush(date) {
         d3.select("#timePickerOuter").select("svg").remove();
         var dateWidth = 50 * date.length <= 600 ? 600 / date.length : 50; //刷选的时间宽度，若是时间的总长度小于容器的宽度，那么就会占满容器，否则返回最小宽度50
         var svg = d3.select('#timePickerOuter')
@@ -59,7 +83,23 @@ $(function () {
                     range.push(d);
             });
 
+            var dtmp = [
+                { genre: 'Sports', sold: 275 },
+                { genre: 'Strategy', sold: 115 },
+                { genre: 'Action', sold: 120 },
+                { genre: 'Shooter', sold: 350 },
+                { genre: 'Other', sold: 150 },
+            ];
+
+            var da = [];
+            dtmp.forEach(function (d, i) {
+                if (d["sold"] >= range[0] && d["sold"] <= range[range.length - 1])
+                    da.push(d);
+            });
             console.log(range);
+
+            console.log(da);
+            drawData(da);
         });
     }
 });
